d1bcc9fca89d36328ab7227b4d811e4e8b738e9b
Nick Dunn <nick.dunn@flosports.tv>
2023-01-06 13:52:40 -0600

    feat(note-details): fetch dynamic data

--- app/routes/notes.$noteId.tsx
+++ app/routes/notes.$noteId.tsx
@@ -1,20 +1,33 @@
-import { Link } from "@remix-run/react"
+import { Link, useLoaderData } from "@remix-run/react"
 import styles from '~/styles/note-details.css'
+import { getStoredNotes } from "~/data/notes"
+import { LoaderFunction } from "@remix-run/node"
+import { Note } from "~/types/notes.types"
 
 export default function NoteDetailsPage() {
+  const note: Note = useLoaderData()
+
   return (
     <main id="note-details">
       <header>
         <nav>
           <Link to="/notes">Back To All Notes</Link>
         </nav>
-        <h1>NOTE TITLE</h1>
+        <h1>{note.title}</h1>
       </header>
       <p id="note-details-content">
-        NOTE CONTENT
+        {note.content}
       </p>
     </main>
   )
+}
+
+export const loader: LoaderFunction = async ({params}) => {
+  const notes = await getStoredNotes()
+  const noteId = params.noteId
+  const selectedNote = notes.find(note => note.id === noteId)
+
+  return selectedNote
 }
 
 export function links() {
